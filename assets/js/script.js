b50f9a4f53782578005379189a72e4b01b32e878
NataliekohNP <s10227870@connect.np.edu.sg>
2022-12-08 15:53:48 +0800

    making of the board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
-function createboard(){
-    //this is to show the contents inside the div
-    const building = "div"
-    for(let length = 0; length<20; length++ ){
-        const row = document.createElement("div");
-        document.getElementById("board").appendChild(row);
-        row.className = "row";
-        for(let height = 0; height<20; height++){
-            const column = document.createElement("div");
-            column.innerHTML= building;
-            column.className="column";
-            row.appendChild(column);
+// function createboard(){
+//     console.log("hello world")
+//     //this is to show the contents inside the div
+//     const building = "div"
+//     for(let length = 0; length<20; length++ ){
+//         var row = document.createElement("div");
+//         document.getElementById("board").append(row);
+//         row.className = "row";
+//         for(let height = 0; height<20; height++){
+//             const column = document.createElement("div");
+//             column.innerHTML= building;
+//             column.className="column";
+//             row.appendChild(column);
+//         }
+//     }
+// }
+// createboard()
+
+
+$().ready(function() {
+    for(let length=0; length<20; length++){
+        var row = document.createElement("div");
+        row.className= "board-row";
+        for(let height=0; height<20; height++){
+            var column = document.createElement("div");
+            column.className="board-column";
+            row.append(column)
         }
+        $("#board").append(row);
     }
-}
-createboard()
+});
