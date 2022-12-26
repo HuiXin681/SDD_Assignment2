02b1fb532f629df0ff2b5548d5c31e8b18c74df8
HuiXin681 <s10223047@connect.np.edu.sg>
2022-12-26 14:26:18 +0800

    Scoring works but scuffed 😔

--- assets/js/GameClass.js
+++ assets/js/GameClass.js
@@ -1,7 +1,7 @@
 export class Game {
     score = 0;
     coins = 16;
-    turns = 0;
+    turns = 1;
     buildings = [];
 
     constructor(buildings = []) {
@@ -48,7 +48,7 @@ export class Game {
                     const option = $(".being-dragged")[0];
 
                     for (const box of boxes) {
-                        if (box.classList.contains("adjacent") || this.turns === 0) {
+                        if (box.classList.contains("adjacent") || this.turns === 1) {
                             let content = "<div class='building'>" + option.innerHTML + "</div>";
 
                             box.classList.add("filled");
@@ -72,9 +72,11 @@ export class Game {
     }
 
     updateTurn() {
+        console.log("Turn: " , this.turns);//toRemove
         this.turns++;
-
+        $(".turn-val").html(String(this.turns));
         this.updateScore();
+        this.updateCoin();
         this.removeOptions();
         this.createOptions();
     }
@@ -84,78 +86,109 @@ export class Game {
         $(".score-val").html(String(this.score));
     }
 
+    updateCoin() {
+        this.coins--;
+        $(".coin-val").html(String(this.coins));
+    }
+
     calculateScore() {
-        for (const cell of $(".board-cell.filled .building")) {
-            const col = cell.id.split("-")[0];
-            const row = cell.id.split("-")[1];
+        this.score = 0;
+        if (this.turns !== 2 ) {
+            for (const cell of $(".board-cell.filled")) {
+                console.log("Building checking : ", cell.innerText)
+                const col = parseInt(cell.id.split("-")[0]);
+                const row = parseInt(cell.id.split("-")[1]);
 
-            let adjacent = [];
-            adjacent.push($("#" + col + "-" + (row - 1)));
-            adjacent.push($("#" + col + "-" + (row + 1)));
-            adjacent.push($("#" + (col - 1) + "-" + row));
-            adjacent.push($("#" + (col + 1) + "-" + row));
 
-            let toAdd = 0;
-            switch (cell.innerHTML) {
-                case "R":
-                    for (const a of adjacent) {
-                        const type = a.innerHTML;
-                        if (type === "I") {
-                            toAdd = 1;
-                            break;
+                let adjacent = [];
+                adjacent.push($("#" + col + "-" + (row - 1)));
+                adjacent.push($("#" + col + "-" + (row + 1)));
+                adjacent.push($("#" + (col - 1) + "-" + row));
+                adjacent.push($("#" + (col + 1) + "-" + row));
+
+                let sameRow = [];
+                sameRow.push($("#" + (col - 1) + "-" + row));
+                sameRow.push($("#" + (col + 1) + "-" + row));
+
+                let toAdd = 0;
+
+                switch (cell.innerText) {
+                    case "R":
+                        for (const a of adjacent) {
+                            const type = a[0].innerText;
+                            console.log("Type: " ,type);//toRemove
+                            if (type === "I") {
+                                toAdd = 1;
+                                break;
+                            }
+                            else if (type === "R" || type === "C") {
+                                toAdd += 1;
+                                break;
+                            }
+                            else if (type === "O") {
+                                toAdd += 2;
+                                break;
+                            }
                         }
-                        else if (type === "R" || type === "C") {
-                            toAdd += 1;
-                            break;
+                        break;
+                    case "I":
+                        toAdd += 1;
+                        for (const a of adjacent) {
+                            const type = a[0].innerText;
+                            console.log("Type: " ,type);//toRemove
+                            if (type === "R") {
+                                this.coins += 1;
+                                break;
+                            }
                         }
-                        else if (type === "O") {
-                            toAdd += 2;
-                            break;
+                        break;
+                    case "C":
+                        for (const a of adjacent) {
+                            const type = a[0].innerText;
+                            console.log(type);//toRemove
+                            if (type === "C") {
+                                toAdd += 1;
+                                break;
+                            }
+                            else if (type === "R") {
+                                this.coins += 1;
+                                break;
+                            }
                         }
-                    }
-                    break;
-                case "I":
-                    toAdd += 1;
-                    for (const a of adjacent) {
-                        const type = a.innerHTML;
-                        if (type === "R") {
-                            this.coins += 1;
-                            break;
+                        break;
+                    case "O":
+                        for (const a of adjacent) {
+                            const type = a[0].innerText;
+                            console.log("Type: " ,type);//toRemove
+                            if (type === "O") {
+                                toAdd += 1;
+                                break;
+                            }
                         }
-                    }
-                    break;
-                case "C":
-                    for (const a of adjacent) {
-                        const type = a.innerHTML;
-                        if (type === "C") {
-                            toAdd += 1;
-                            break;
-                        } else if (type === "R") {
-                            this.score += 1;
-                            break;
+                        break;
+                    //Need to confirm about point system - if points are counted per connection or per road
+                    case "*":
+                        for (const sR of sameRow) {
+                            console.log(sR.ID);//toRemove
+                            const type = sameRow.innerText;
+                            if (type == "*") {
+                                toAdd += 1;
+                                break;
+                            }
                         }
-                    }
-                    break;
-                case "O":
-                    for (const a of adjacent) {
-                        const type = a.innerHTML;
-                        if (type === "O") {
-                            toAdd += 1;
-                            break;
-                        }
-                    }
-                    break;
-                case "*":
-                    break;
+                        break;
+                }
+                this.score += toAdd;
+                console.log("ToAdd" , toAdd);//toRemove
+                console.log("Current Score" , this.score);//toRemove
             }
-
-            this.score += toAdd;
         }
     }
 
     createOptions() {
         for (let i = 0; i < 2; i++) {
             const building = this.buildings[Math.floor(Math.random() * this.buildings.length)].type;
+            //const building = this.buildings[0].type;//toRemove
             $(".building-container").append(
                 $(document.createElement('div'))
                     .addClass("option")
