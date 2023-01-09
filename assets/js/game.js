e3374230d0bd51cf1ecdee1df8c7edaaf04f2bb9
jstnde <nahcnitsuj@gmail.com>
2023-01-10 00:16:57 +0800

    add building information

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,46 @@
 import {Game} from "./GameClass.js";
 import {Firebase} from "./firebase.js";
 
-$(function() {
-    let game;
-    let firebase = new Firebase();
+$(function () {
+	let game;
+	let firebase = new Firebase();
 
-    $.ajax({
-        url: "/assets/data/buildings.json",
-        data: {},
-        success: gameHandler,
-        cache: false,
-        dataType: "json",
-        type: "GET"
-    });
+	$.ajax({
+		url: "/assets/data/buildings.json",
+		data: {},
+		success: gameHandler,
+		cache: false,
+		dataType: "json",
+		type: "GET"
+	});
 
-    function gameHandler(response) {
-        game = new Game(response.buildings);
-        game.start();
-    }
-    
-    $("#form").submit(function(e){
-        e.preventDefault();
-        game.name = $("#username").val();
-        console.log("submitting");
-        firebase.init(game);
-    });
+	function gameHandler(response) {
+		const modalBody = $(".modal-body");
+		response.buildings.forEach(b => {
+			const img = document.createElement("img");
+			const info = document.createElement("div");
+			const title = document.createElement("div");
+			const desc = document.createElement("div");
+			img.src = `assets/img/${b.image}`;
+			info.append(img);
+			title.classList.add("modal-body_title");
+			title.append(`${b.name} (${b.type})`);
+			info.append(title);
+			desc.classList.add("modal-body_desc");
+			desc.append(b.description);
+			info.append(desc);
+			info.classList.add("modal-body_information");
+			modalBody.append(info);
+		});
+
+		game = new Game(response.buildings);
+		game.start();
+	}
+
+	$("#form").submit(function (e) {
+		e.preventDefault();
+		game.name = $("#username").val();
+		console.log("submitting");
+		firebase.init(game);
+	});
 });
